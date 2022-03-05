090e5fe0f7e4e08a8ac880f6763e60220af5c6a1
mat37dev <59926286+mat37dev@users.noreply.github.com>
2022-03-05 17:11:45 +0100

    test

---
+++ src/main/java/frm/mat_37/murder/murder/Murder.java
@@ -0,0 +1,17 @@
+package frm.mat_37.murder.murder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Murder extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvent(new );
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
